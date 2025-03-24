9fb395eadeab0be1b6e281d004f27d7d648bc477
abdrhmn09 <abdurrahmanmarzuki2@gmail.com>
2025-03-24 18:16:48 +0700

    edit inventaris

--- js/userInventory.js
+++ js/userInventory.js
@@ -1,3 +1,12 @@
+// Fetch and insert navbar
+fetch("/component/userNavbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar-container").innerHTML = data;
+    markActiveNavLink(); // Call function to mark active link after navbar is loaded
+    setupNavbarInteractions(); // Setup navbar interactions after navbar is loaded
+  })
+  .catch((err) => console.error("Error mengambil navbar:", err));
 // script.js
 document.addEventListener('DOMContentLoaded', function () {
     // Pastikan elemen search sudah ada di HTML dengan id "search-input"
