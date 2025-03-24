475749d0c7dcb298a4ae35ae0eb5a16fb0e05678
Muadz <muadzfauzi@mhs.usk.ac.id>
2025-03-24 14:46:20 +0700

    update

--- js/script-peminjamanUser.js
+++ js/script-peminjamanUser.js
@@ -7,22 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
     markActiveNavLink(); // Call function to mark active link after navbar is loaded
     setupNavbarInteractions(); // Setup navbar interactions after navbar is loaded
     })
-    
+
 .catch((err) => console.error("Error mengambil navbar:", err));
     document.getElementById('formPeminjaman').addEventListener('submit', function(event) {
         event.preventDefault();
         
         let nama = document.getElementById('nama').value;
         let barang = document.getElementById('barang').value;
+        let jumlah = document.getElementById('jumlah').value;
         let tanggal = document.getElementById('tanggal').value;
         
-        if (nama && barang && tanggal) {
+        if (nama && barang && jumlah && tanggal) {
             alert('Peminjaman sedang diproses...');
             
             setTimeout(() => {
                 let riwayatData = document.getElementById('riwayatData');
                 let newRow = riwayatData.insertRow();
-                newRow.innerHTML = `<td>${nama}</td><td>${barang}</td><td>${tanggal}</td><td>Diproses</td>`;
+                newRow.innerHTML = `<td>${nama}</td><td>${barang}</td><td>${jumlah}</td><td>${tanggal}</td><td>Diproses</td>`;
                 
                 document.getElementById('formPeminjaman').reset();
                 alert('Peminjaman berhasil diajukan!');
